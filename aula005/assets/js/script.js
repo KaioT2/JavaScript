46ee78fb5571d3738c0c1f4d2c75bdd845fbfbba
Kaio Dias <kaiofd20062@gmail.com>
2022-12-16 17:31:56 -0300

    Variáveis e tipos de dados
    Nessa aula aprendi os principais tipos primitivos, como declarar variáveis  e também como exibir coisas no console.

---
+++ aula005/assets/js/script.js
@@ -0,0 +1,25 @@
+//Variáveis - let nomedavariavel = oquerecebe
+
+    let nome = "Kaio"
+    let idade = 16
+
+//Exibir no console - console.log
+
+    console.log(nome)
+    console.log(idade)
+
+//Tipos de dados
+
+    let name = "Juca" //String
+
+    let age = 28 //Number
+
+    let ligado = true //Boolean
+
+    let lista = ["ovo", "farinha", "leite"] //Array ou Vetor
+
+    let completeName = {name: "Kaio", lastname: "Dias"} //Objeto
+
+//Exibir o tipo da variável no console - console.log(typeof nomedavariavel)
+
+console.log(typeof lista)
