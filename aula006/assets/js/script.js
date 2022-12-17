263cf1a5a01ec486aeb22eb28d578b31a41908c3
Kaio Dias <kaiofd20062@gmail.com>
2022-12-17 11:06:21 -0300

    Aulas 06 - 08
    Nas aulas 6 e 7 foi abordado superficialmente como criar variáveis já na 8 falou um pouco sobre operações com variáveis. Não coloquei o arquivo da aula 8 pois já sabia o que foi abordado e por serem conceitos bem básicos.

---
+++ aula006/assets/js/script.js
@@ -0,0 +1,32 @@
+//Como criar variáveis
+
+//Pode-se usar: let - var - const
+
+//const = é uma constante, ou seja, não há como modificar ela durante a execução do programa
+
+/* Funciona 
+
+    let nome = "Kaio"
+    console.log(nome)
+
+    nome = "João"
+    console.log(nome)
+
+================================
+Funciona
+
+    var nome = "Kaio"
+    console.log(nome)
+
+    nome = "João"
+    console.log(nome)
+
+================================ */
+
+//Não funciona pois 'nome' foi criado como uma constante e nesse caso não pode ser modificado 
+
+    const nome = "Kaio"
+    console.log(nome)
+
+    nome = "João"
+    console.log(nome)
