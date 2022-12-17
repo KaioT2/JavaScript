7b0fd94930bef2a8ca1c1f74de7b32ca98226366
Kaio Dias <kaiofd20062@gmail.com>
2022-12-17 11:33:24 -0300

    String e Template String

--- aula007/assets/js/script.js
+++ aula007/assets/js/script.js
@@ -1,1 +1,15 @@
+//String e Templete Strings
 
+/*Template string é uma "Super String", nela você pode fazer a concatenação de uma variável com 
+o texto apenas usando o placeholder (denominação para '${}') */
+
+let nome = "Kaio";
+let sobrenome = "Dias";
+
+//Ex.:
+
+//let nomeCompleto = 'Meu nome é ' + nome + ' ' + sobrenome;
+
+let nomeCompleto = `Meu nome é ${nome} ${sobrenome}`; //Isso é um template string
+
+console.log(nomeCompleto);
