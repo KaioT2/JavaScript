712b295695018346aba7bc6c1bab11e433ebbeec
Kaio Dias <kaiofd20062@gmail.com>
2022-12-18 09:15:05 -0300

    Aula sobre Switch

---
+++ aula011/assets/js/script.js
@@ -0,0 +1,22 @@
+// Switch
+
+let profession = "fiscal";
+
+switch(profession){
+    case 'fiscal' : 
+        console.log('Sua camisa será VERDE');
+    break;
+
+    case 'policial' : 
+        console.log('Sua camisa será AZUL');
+    break;
+
+    case 'bombeiro' : 
+        console.log('Sua camisa será VERMELHO');
+    break;
+
+    default:
+        console.log('Sua camisa será PRETA');
+    break;
+}
+
