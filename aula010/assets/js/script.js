12873bb9813bb4d512b43c0f988fc46ce3f0a202
Kaio Dias <kaiofd20062@gmail.com>
2022-12-18 09:05:45 -0300

    Condicional ternário

---
+++ aula010/assets/js/script.js
@@ -0,0 +1,12 @@
+//Condicional ternário
+
+//É basicamente um if em uma linha 
+
+//Estrutura: condição ? valor se verdadeiro : valor se falso
+
+let isMember = true ;
+
+let shipping = isMember ? 0.0 : 10.0;
+
+console.log("Frete: "+shipping);
+
