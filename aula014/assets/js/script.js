d8762973bd03ab999a5fac5192bf3b19b4c916fe
Kaio Dias <kaiofd20062@gmail.com>
2022-12-19 16:47:42 -0300

    Arrow functions

---
+++ aula014/assets/js/script.js
@@ -0,0 +1,19 @@
+//Arrow Function
+
+/*Basicamente é uma forma "resumida" de fazer uma função 
+
+Ex: Função normal:
+
+function somar(x,y){
+    return x+y;
+}
+
+console.log(somar(10,5));
+
+*/
+
+//Agora com arrow funtions 
+
+let somar = (x, y) => x + y;
+
+console.log(somar(10,5));
