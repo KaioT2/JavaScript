09ae35d2f2b1ea66b1cde5a128da7709f5ffd219
Kaio Dias <kaiofd20062@gmail.com>
2022-12-20 09:49:42 -0300

    Array e operações com array

---
+++ aula016/assets/js/script.js
@@ -0,0 +1,29 @@
+//Array e operações com array
+
+let ingredientes = ['leite', 'farinha', 'ovo', 'açúcar'];
+
+/*
+Função 'push' (adiciona um item ao array)
+
+ex:
+ingredientes.push('chocolate');
+
+==============================================
+
+Função 'pop' (remove o último item do array)
+
+ex:
+ingredientes.pop();
+
+==============================================
+
+Função 'shift' (remove o primeiro item do array)
+
+ex: 
+ingredientes.shift();
+*/
+
+console.log(ingredientes.length);
+
+
+
