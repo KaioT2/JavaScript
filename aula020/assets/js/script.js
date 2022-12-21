837b97773252f3a5dfe25b54bf10082767bdfdf0
Kaio Dias <kaiofd20062@gmail.com>
2022-12-21 12:25:33 -0300

    Funções básicas de array

---
+++ aula020/assets/js/script.js
@@ -0,0 +1,31 @@
+//Funções de array
+
+let fruits = ['Maça', 'Uva', 'Laranja', 'Banana'];
+
+/*
+Comando "shift" retira o primeiro item do array
+
+fruits.shift();
+
+===================================================================
+
+Comando "pop" retira o ultimo item do array
+
+fruits.pop();
+
+===================================================================
+
+Comando "push" coloca um item no array
+
+fruits.push('Pera');
+
+===================================================================
+
+Comando "join" junta os itens do array em uma string com um valor entre cada item. (o que colocar entre o parênteses vai estar entre os itens)
+*/
+console.log(fruits.join('-')); 
+
+//resultado: Maça-Uva-Laranja-Banana
+
+
+
