2551e1441dae686ddb5d47fa9e8d8a70e97b0efa
Kaio Dias <kaiofd20062@gmail.com>
2022-12-22 12:54:41 -0300

    DOM - Selecionar elementos

---
+++ aula023/assets/js/script.js
@@ -0,0 +1,21 @@
+//Eventos e manipulação do DOM
+
+//Selecionar elementos através da tag:
+
+let gg = document.getElementsByTagName("h1");
+console.log(gg);
+
+//Selecionar elementos através do id:
+
+let hh = document.getElementById("teste");
+console.log(hh);
+
+//Selecionar elementos através da clase:
+
+let ii = document.getElementsByClassName("botao");
+console.log(ii);
+
+//Selecionar por qualquer coisa:
+
+let jj = document.querySelectorAll("#teste ul li")
+console.log(jj);
