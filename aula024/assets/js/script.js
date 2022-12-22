98006cc46396986e776554af71f2cc3fd31bd4a1
Kaio Dias <kaiofd20062@gmail.com>
2022-12-22 14:27:35 -0300

    DOM - Eventos

---
+++ aula024/assets/js/script.js
@@ -0,0 +1,34 @@
+//Eventos e manipulação do DOM
+/*
+//Selecionar elementos através da tag:
+
+let gg = document.getElementsByTagName("h1");
+console.log(gg);
+
+//Selecionar elementos através do id:
+
+let hh = document.getElementById("teste");
+console.log(hh);
+
+//Selecionar elementos através da clase:
+
+let ii = document.getElementsByClassName("botao");
+console.log(ii);
+
+//Selecionar por qualquer coisa:
+
+let jj = document.querySelectorAll("#teste ul li")
+console.log(jj);
+
+//EVENTOS DE CLICK
+
+//onclick... (quando clicar) pode ser definido no elemento ou no arquivo js
+
+let botao = document.querySelector('.botao');
+
+addEventListener("click", () => clicou());
+*/
+
+function clicou(){
+    alert("Clicou no botão");
+};
