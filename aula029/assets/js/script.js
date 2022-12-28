edc2efb011d9cc46e31e7cc5c389a0b59070a0f1
Kaio Dias <kaiofd20062@gmail.com>
2022-12-28 09:44:11 -0300

    DOM - Eventos de teclado

---
+++ aula029/assets/js/script.js
@@ -0,0 +1,27 @@
+//Eventos de teclado
+
+/*
+      function apertou(){
+            console.log("APERTOU");
+      }
+
+      function segurou(){
+            console.log("SEGUROU");
+      }
+
+function soltou(e){
+      console.log(e.code); 
+      console.log(e.ctrlKey);
+      console.log(e.shiftKey);
+      console.log(e.altKey);
+}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
+*/
+
+function clicou(){
+      const input = document.querySelector('input');
+      
+      input.style.backgroundColor = '#f45';
+}
